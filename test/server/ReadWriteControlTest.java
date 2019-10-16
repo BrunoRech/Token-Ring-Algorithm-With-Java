212324df4f2173fa1b4a9c5742c1f2ca548c8681
jefip <jefipnz@gmail.com>
2019-10-16 20:20:10 -0300

    Desenvolvida Escrita/Leitura do arquivo.

---
+++ test/server/ReadWriteControlTest.java
@@ -0,0 +1,41 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jeferson Penz
+ */
+public class ReadWriteControlTest {
+
+    /**
+     * Test of run method, of class ReadWriteControl.
+     */
+    @Test
+    public void testRun() {
+        File file = new File("Teste.txt");
+        try {
+            file.createNewFile();
+            ReadWriteControl control = new ReadWriteControl(file.getAbsolutePath());
+            (new Thread(control)).start();
+            assertEquals(control.getAllData(), "");
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {}
+                control.addData("Teste de escrita no arquivo: " + i + "\n");
+                for (int j = 0; j < 100; j++) {
+                    control.addData("Teste");
+                }
+                control.addData("\n");
+            }
+            assertNotEquals("", control.getAllData());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+}
