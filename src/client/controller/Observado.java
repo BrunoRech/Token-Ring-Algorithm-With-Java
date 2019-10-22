4f53ed8ed0c2bcdf2bd1304b7c55e83e8c8f9924
jefip <jefipnz@gmail.com>
2019-10-22 19:50:49 -0300

    Limpar arquivos extras.

--- src/client/controller/Observado.java
+++
@@ -1,6 +0,0 @@
-package client.controller;
-
-//interface do observado com a declaracao de cada metodo que sera usado
-public interface Observado {
-	void addObservador(Observador obs);
-}
--- src/client/controller/Observador.java
+++
@@ -1,6 +0,0 @@
-package client.controller;
-
-//interface do observador com a declaracao de cada metodo de notificacao que sera usado
-public interface Observador {
-    void notifyTokenReceived();
-}
